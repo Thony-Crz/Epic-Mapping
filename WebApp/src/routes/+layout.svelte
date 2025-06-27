da04f9068a6fbd47b6b2ca85afab2f3d7c5da2d8
Anthony CRIBLEZ <acriblez@valsoftware.com>
2025-06-27 22:59:34 +0200

    init and fix tailwind

--- WebApp/src/routes/+layout.svelte
+++ WebApp/src/routes/+layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  import BlueCard from '$ui/components/BlueCard.svelte';
+  import '../app.css';
 </script>
 
-<div class="p-8 space-y-4">
-  <BlueCard title="Connexion utilisateur" />
-  <BlueCard title="Gestion des mappings Azure DevOps" />
-</div>
+<slot />
--- WebApp/src/routes/+page.svelte
+++ WebApp/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import BlueCard from '$ui/components/BlueCard.svelte';
+</script>
+
+<div class="p-8 space-y-4">
+  <BlueCard title="Connexion utilisateur" />
+  <BlueCard title="Gestion des mappings Azure DevOps" />
+</div>
