93ef0fb450e9cc42d28904d66d55f797ad1b0864
Anthony CRIBLEZ <acriblez@valsoftware.com>
2025-06-29 14:16:02 +0200

    fix : no sticky header

--- WebApp/src/routes/+layout.svelte
+++ WebApp/src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <!-- Header -->
-<header class="bg-white shadow-sm border-b border-gray-200 sticky top-0 z-10">
+<header class="bg-white shadow-sm border-b border-gray-200 z-10">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="flex justify-between items-center h-16">
       <!-- Logo/Titre -->
