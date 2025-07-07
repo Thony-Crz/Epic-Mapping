f25dcf2213ebf4c9b99230381e7d9840b0657e76
Anthony CRIBLEZ <acriblez@valsoftware.com>
2025-07-07 21:20:59 +0200

    Domain : Auth

---
+++ backEnd/src/Domain/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthResult> AuthenticateAsync(string username, string password);
+        Task<AuthResult> AuthenticateWithOAuthAsync(string provider, string code);
+    }
+}
