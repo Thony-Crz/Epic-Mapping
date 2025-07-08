61b35a979fdf2b08533db10c671d2d9d585e963f
Anthony CRIBLEZ <acriblez@valsoftware.com>
2025-07-08 08:31:43 +0200

    rename controllers

--- backEnd/EpicMapping.WebApi/Controllers/ExternalAuthController.cs
+++ backEnd/EpicMapping.WebApi/Controllers/ExternalAuthController.cs
@@ -1,14 +1,13 @@
 ﻿using Application.UseCases.AuthenticateUser;
 using FluentValidation;
 using MediatR;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EpicMapping.WebApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AuthController(IMediator mediator) : ControllerBase
+    public class ExternalAuthController(IMediator mediator) : ControllerBase
     {
         [HttpPost("authenticate")]
         public async Task<IActionResult> Authenticate([FromBody] AuthorizeExternalProviderCommand command)
